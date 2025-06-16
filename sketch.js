f060c7e6507a1fffcf051d50ff9088b8015b3dec
YY31-z <yuyanzang@gmail.com>
2025-06-16 13:29:10 +0200

    EMOLAND

--- sketch.js
+++ sketch.js
@@ -4,7 +4,7 @@ let parola = "EMOLAND";
 let myFont;
 
 function preload() {
- myFont = loadFont("./fonts/EMOLAND REGULAR(1).ttf");
+myFont = loadFont("./fonts/EMOLAND REGULAR(1).ttf");
 }
 
 function setup() {
@@ -14,6 +14,7 @@ textAlign(CENTER, CENTER);
 textSize(70);
 fill(0);
 textFont(myFont);
+
 svgPaths = [
 "./assets/grafici/1.svg",
 "./assets/grafici/2.svg",
@@ -22,18 +23,42 @@ svgPaths = [
 "./assets/grafici/5.svg",
 "./assets/grafici/6.svg",
 "./assets/grafici/7.svg",
-"./assets/grafici/8.svg",
+"./assets/grafici/9.svg",
+"./assets/grafici/10.svg",
+"./assets/grafici/11.svg",
+"./assets/grafici/12.svg",
+"./assets/grafici/13.svg",
+"./assets/grafici/14.svg",
+"./assets/grafici/15.svg",
+"./assets/grafici/16.svg",
+"./assets/grafici/17.svg",
+"./assets/grafici/18.svg",
+"./assets/grafici/19.svg",
+"./assets/grafici/20.svg",
+"./assets/grafici/21.svg",
+"./assets/grafici/22.svg",
+"./assets/grafici/23.svg",
+"./assets/grafici/24.svg",
+"./assets/grafici/25.svg",
+"./assets/grafici/26.svg",
+"./assets/grafici/27.svg",
+"./assets/grafici/28.svg",
+"./assets/grafici/29.svg",
+"./assets/grafici/30.svg",
+"./assets/grafici/31.svg",
+"./assets/grafici/32.svg",
 ];
 injectDropCSS();
+
 // ✅ 每隔 0.5 秒生成一个图像
 setInterval(() => {
 let index = floor(random(svgPaths.length));
-let x = random(10, windowWidth +300);
+let x = random(50, windowWidth - 150);
 addDroppingSVG(svgPaths[index], x);
 }, 500);
 }
 function draw() {
-background("#F7E4E7");
+background("white");
 fill(0);
 noStroke(); text(parola, width / 2, height/2);
 }
@@ -41,8 +66,8 @@ noStroke(); text(parola, width / 2, height/2);
 // ✅ 创建并播放掉落动画
 function addDroppingSVG(path, x) {
 let img = createImg(path);
-img.size(300,300);
-img.position(x, 100); // 从上方掉落
+img.size(200,200);
+img.position(x, -120); // 从上方掉落
 img.addClass("drop-anim");
 // 动画结束后删除图像
 setTimeout(() => {
